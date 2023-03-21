9b3a5d1c447d0fa21cf070d0f7f13e7071d312a1
杨晓城 <yangxiaocheng@zhilingsd.com>
2023-03-21 20:28:18 +0800

    修复：doAfterInitialized不执行（百度sdk自7.5.4开始不再发送鉴权广播）

--- BaiduLib/src/main/java/com/naruto/lib/baidu/BaiduSdkInitializer.kt
+++ BaiduLib/src/main/java/com/naruto/lib/baidu/BaiduSdkInitializer.kt
@@ -5,10 +5,18 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.util.Log
 import com.baidu.location.LocationClient
 import com.baidu.mapapi.SDKInitializer
+import com.baidu.mapapi.VersionInfo
+import com.baidu.mapapi.search.core.SearchResult
+import com.baidu.mapapi.search.weather.WeatherDataType
+import com.baidu.mapapi.search.weather.WeatherSearch
+import com.baidu.mapapi.search.weather.WeatherSearchOption
 import com.naruto.lib.common.commonLibInit
 import com.naruto.lib.common.utils.LogUtils
+import java.util.*
+import kotlin.concurrent.schedule
 
 /**
  * @Description
@@ -18,42 +26,75 @@ import com.naruto.lib.common.utils.LogUtils
  */
 class BaiduSdkInitializer {
     companion object {
-        private var isBaiduInitialized: Boolean? = null
+        private var isBaiduKeyValid: Boolean? = null
         private val taskQueue: MutableList<(Boolean) -> Unit> = mutableListOf()
 
         fun doAfterInitialized(block: (Boolean) -> Unit) {
-            isBaiduInitialized?.run(block) ?: taskQueue.add(block)
+            isBaiduKeyValid?.run(block) ?: taskQueue.add(block)
+        }
+
+        internal fun startInitializedListener(context: Context) {
+            (if (VersionInfo.getApiVersion() > "7_5_3") Checker() else Receiver { context }).start()
         }
     }
 
     /**
-     * @Description
+     * @Description 监听鉴权广播（7.5.4及以上版本无效）
      * @Author Naruto Yang
      * @CreateDate 2022/8/27 0027
      * @Note
      */
-    internal class BaiduInitializerReceiver : BroadcastReceiver() {
+    private class Receiver(private val context: () -> Context) :
+        BroadcastReceiver(), BaiduInitializerListener {
 
         override fun onReceive(context: Context, intent: Intent) {
             LogUtils.i("--->action=${intent.action}")
-            isBaiduInitialized =
-                intent.action == SDKInitializer.SDK_BROADTCAST_ACTION_STRING_PERMISSION_CHECK_OK
-            while (taskQueue.isNotEmpty()) {
-                taskQueue.removeAt(0).invoke(isBaiduInitialized!!)
-            }
-            unRegister(context)
+            onFinish(intent.action == SDKInitializer.SDK_BROADTCAST_ACTION_STRING_PERMISSION_CHECK_OK)
+            context.unregisterReceiver(this)
         }
 
-        fun register(context: Context) {
+        override fun start() {
             val intentFilter = IntentFilter().apply {
                 addAction(SDKInitializer.SDK_BROADTCAST_ACTION_STRING_PERMISSION_CHECK_ERROR)
                 addAction(SDKInitializer.SDK_BROADTCAST_ACTION_STRING_PERMISSION_CHECK_OK)
             }
-            context.registerReceiver(this, intentFilter)
+            context().registerReceiver(this, intentFilter)
+        }
+    }
+
+    /**
+     * @Description 循环检查鉴权结果
+     * @Author Naruto Yang
+     * @CreateDate 2023/3/21 0021
+     * @Note
+     */
+    private class Checker : BaiduInitializerListener {
+        private val weatherSearch = WeatherSearch.newInstance().apply {
+            setWeatherSearchResultListener { result ->
+                when (result.error) {
+                    SearchResult.ERRORNO.PERMISSION_UNFINISHED -> Timer().schedule(300) { start() }
+                    SearchResult.ERRORNO.NETWORK_ERROR, SearchResult.ERRORNO.NETWORK_TIME_OUT ->
+                        onFinish(null)
+                    else -> onFinish(result.error != SearchResult.ERRORNO.KEY_ERROR)
+                }
+            }
         }
 
-        fun unRegister(context: Context) {
-            context.unregisterReceiver(this)
+        private val option = WeatherSearchOption()
+            .apply { weatherDataType(WeatherDataType.WEATHER_DATA_TYPE_ALL).districtID("110105") }// 天安门区域ID
+
+        override fun start() {
+            Log.i("naruto", "--->startCheck")
+            weatherSearch.request(option)
+        }
+    }
+
+
+    private interface BaiduInitializerListener {
+        fun start()
+        fun onFinish(isValid: Boolean?) {
+            isBaiduKeyValid = isValid
+            while (taskQueue.isNotEmpty()) taskQueue.removeAt(0).invoke(isBaiduKeyValid ?: false)
         }
     }
 }
@@ -65,7 +106,7 @@ class BaiduSdkInitializer {
 fun Application.baiduLibInit() {
     if (hasInitialized) return
     hasInitialized = true
-    BaiduSdkInitializer.BaiduInitializerReceiver().register(this)
+    BaiduSdkInitializer.startInitializedListener(this)
     SDKInitializer.setAgreePrivacy(applicationContext, true)
     SDKInitializer.initialize(applicationContext)
     LocationClient.setAgreePrivacy(true)
